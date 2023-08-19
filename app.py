7766432af17375c6586fb0ff260430c307973f9f
Takeshi Saitoh <saitoh@ces.kyutech.ac.jp>
2023-08-19 23:45:47 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,76 @@
+import streamlit as st
+import cv2
+import numpy as np
+import av
+import mediapipe as mp
+from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
+
+st.title("Streamlit App: Face motion by MediaPipe")
+st.write("Kyutech, Saitoh-lab")
+
+def func(value1, value2):
+    return int(value1 * value2)
+
+def process(image, is_show_image, draw_pattern):
+    out_image = image.copy()
+
+    with mp.solutions.face_mesh.FaceMesh(
+        static_image_mode=True,
+        max_num_faces=1,
+        refine_landmarks=True,
+        min_detection_confidence=0.5
+    ) as face_mesh:
+
+        results = face_mesh.process(image)
+
+        (image_height, image_width) = image.shape[:2]
+
+        black_image = np.zeros((image_height, image_width, 3), np.uint8)
+        white_image = black_image + 200
+
+        if is_show_image == False:
+            out_image = white_image.copy()
+
+        if draw_pattern == "A":
+            if results.multi_face_landmarks:
+                for face in results.multi_face_landmarks:
+                   for landmark in face.landmark:               
+                        x = int(landmark.x * image_width)
+                        y = int(landmark.y * image_height)
+                        cv2.circle(out_image, center=(x, y), radius=2, color=(0, 255, 0), thickness=-1)
+                        cv2.circle(out_image, center=(x, y), radius=1, color=(255, 255, 255), thickness=-1)
+
+    return cv2.flip(out_image, 1)
+
+
+RTC_CONFIGURATION = RTCConfiguration(
+    {"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]}
+)
+
+
+class VideoProcessor:
+    def __init__(self) -> None:
+        self.is_show_image = True
+        self.draw_pattern = "A"
+
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+
+        img = process(img, self.is_show_image, self.draw_pattern)
+
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
+
+
+webrtc_ctx = webrtc_streamer(
+    key="example",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration=RTC_CONFIGURATION,
+    media_stream_constraints={"video": True, "audio": False},
+    video_processor_factory=VideoProcessor,
+    async_processing=True,
+)
+
+
+if webrtc_ctx.video_processor:
+    webrtc_ctx.video_processor.is_show_image = st.checkbox("show camera image", value=True)
+    webrtc_ctx.video_processor.draw_pattern = st.radio("select draw pattern", ["A", "None"], key="A", horizontal=True)
