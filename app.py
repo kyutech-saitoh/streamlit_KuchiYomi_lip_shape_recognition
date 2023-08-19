559e2d78f27f848d2e1cf3d2bf5daedaf4901e49
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-20 00:33:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,80 +16,6 @@ video_data = st.file_uploader("Upload file", ['mp4','mov', 'avi'])
 temp_file_to_save = './temp_file_1.mp4'
 temp_file_result  = './temp_file_2.mp4'
 
-# func to save BytesIO on a drive
-def write_bytesio_to_file(filename, bytesio):
-    """
-    Write the contents of the given BytesIO to a file.
-    Creates the file or overwrites the file if it does
-    not exist yet. 
-    """
-    with open(filename, "wb") as outfile:
-        # Copy the BytesIO stream to the output file
-        outfile.write(bytesio.getbuffer())
-
-if video_data:
-    # save uploaded video to disc
-    write_bytesio_to_file(temp_file_to_save, video_data)
-
-    # read it with cv2.VideoCapture(), 
-    # so now we can process it with OpenCV functions
-    cap = cv2.VideoCapture(temp_file_to_save)
-
-    # grab some parameters of video to use them for writing a new, processed video
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    frame_fps = cap.get(cv2.CAP_PROP_FPS)  ##<< No need for an int
-    st.write(width, height, frame_fps)
-    
-    # specify a writer to write a processed video to a disk frame by frame
-    fourcc_mp4 = cv2.VideoWriter_fourcc(*'mp4v')
-    out_mp4 = cv2.VideoWriter(temp_file_result, fourcc_mp4, frame_fps, (width, height),isColor = False)
-   
-    while True:
-        ret,frame = cap.read()
-        if not ret: break
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) ##<< Generates a grayscale (thus only one 2d-array)
-        out_mp4.write(gray)
-    
-    ## Close video files
-    out_mp4.release()
-    cap.release()
-
-    ## Reencodes video to H264 using ffmpeg
-    ##  It calls ffmpeg back in a terminal so it fill fail without ffmpeg installed
-    ##  ... and will probably fail in streamlit cloud
-    convertedVideo = "./testh264.mp4"
-    subprocess.call(args=f"ffmpeg -y -i {temp_file_result} -c:v libx264 {convertedVideo}".split(" "))
-    
-    ## Show results
-    col1,col2 = st.columns(2)
-    col1.header("Original Video")
-    col1.video(temp_file_to_save)
-    col2.header("Output from OpenCV (MPEG-4)")
-    col2.video(temp_file_result)
-    col2.header("After conversion to H264")
-    col2.video(convertedVideo)
-
-"""
-if f is not None:
-    tfile = tempfile.NamedTemporaryFile(delete=False) 
-    tfile.write(f.read())
-
-    vf = cv2.VideoCapture(tfile.name)
-
-    stframe = st.empty()
-
-    while vf.isOpened():
-        ret, frame = vf.read()
-        # if frame is read correctly ret is True
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-            
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        stframe.image(gray)
-"""
-
 def func(value1, value2):
     return int(value1 * value2)
 
@@ -123,6 +49,64 @@ def process(image, is_show_image, draw_pattern):
                         cv2.circle(out_image, center=(x, y), radius=1, color=(255, 255, 255), thickness=-1)
 
     return cv2.flip(out_image, 1)
+
+# func to save BytesIO on a drive
+def write_bytesio_to_file(filename, bytesio):
+    """
+    Write the contents of the given BytesIO to a file.
+    Creates the file or overwrites the file if it does
+    not exist yet. 
+    """
+    with open(filename, "wb") as outfile:
+        # Copy the BytesIO stream to the output file
+        outfile.write(bytesio.getbuffer())
+
+if video_data:
+    # save uploaded video to disc
+    write_bytesio_to_file(temp_file_to_save, video_data)
+
+    # read it with cv2.VideoCapture(), 
+    # so now we can process it with OpenCV functions
+    cap = cv2.VideoCapture(temp_file_to_save)
+
+    # grab some parameters of video to use them for writing a new, processed video
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    frame_fps = cap.get(cv2.CAP_PROP_FPS)  ##<< No need for an int
+    st.write(width, height, frame_fps)
+    
+    # specify a writer to write a processed video to a disk frame by frame
+    fourcc_mp4 = cv2.VideoWriter_fourcc(*'mp4v')
+    out_mp4 = cv2.VideoWriter(temp_file_result, fourcc_mp4, frame_fps, (width, height), isColor = False)
+   
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        out_image = process(frame, is_show_image, draw_pattern):
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) ##<< Generates a grayscale (thus only one 2d-array)
+        out_mp4.write(out_image)
+    
+    ## Close video files
+    out_mp4.release()
+    cap.release()
+
+    ## Reencodes video to H264 using ffmpeg
+    ##  It calls ffmpeg back in a terminal so it fill fail without ffmpeg installed
+    ##  ... and will probably fail in streamlit cloud
+    convertedVideo = "./testh264.mp4"
+    subprocess.call(args=f"ffmpeg -y -i {temp_file_result} -c:v libx264 {convertedVideo}".split(" "))
+    
+    ## Show results
+    col1, col2 = st.columns(2)
+    col1.header("Original Video")
+    col1.video(temp_file_to_save)
+    col2.header("Output from OpenCV (MPEG-4)")
+    col2.video(temp_file_result)
+    col2.header("After conversion to H264")
+    col2.video(convertedVideo)
 
 
 RTC_CONFIGURATION = RTCConfiguration(
