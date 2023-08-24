d33f27abc2d49f54a6d6ef697536ecf8d67f3573
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-24 11:01:09 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,74 @@
+import streamlit as st
+import torch
+from PIL import Image
+from torchvision import transforms
+
+st.title("Streamlit App: Mouth shape recognition")
+st.write("Kyutech, Saitoh-lab")
+
+parser = argparse.ArgumentParser(description='lipreading')
+parser.add_argument('arg1', help='INPUT IMAGE PATH')
+args = parser.parse_args()
+
+
+def preprocess(image_path, transform):
+    #########################################
+    # Plese write extracting lipROI process #
+    #########################################
+    
+    image = Image.open(image_path)
+    image = transform(image)  # PIL
+    C, H, W = image.shape
+    image = image.reshape(1, C, H, W)
+    
+    return image
+
+
+def test(model, crop_image):
+
+    # モデルを評価モードにする
+    model.eval()
+
+    with torch.no_grad():
+        # 予測
+        outputs = model(crop_image)
+
+        # 予測結果をクラス番号に変換
+        _, predicted = torch.max(outputs, 1)
+
+    return predicted
+
+
+def main():
+    # data transform
+    transform = transforms.Compose([
+        transforms.Resize((160, 160)),
+        transforms.ToTensor(),
+    ])
+
+    # vowel dict
+    idxtovowel = {0: '閉口', 1: 'あ', 2: 'い', 3: 'う', 4: 'え', 5: 'お'}
+    # training model path
+    model_path = "model/model_mobilenetv2.pth"
+    # load model
+    model = torch.load(model_path)
+    # load device : cpu
+    device = torch.device("cpu")
+    model.to(device)
+
+    image_data = st.file_uploader("Upload file", ['jpg','png'])
+
+    input_image = None
+    
+    if image_data is not None:
+        image = Image.open(uploaded_file)
+        img_array = np.array(image)
+        st.image(img_array, caption = 'uploaded image', use_column_width = None)
+        input_image = pil2cv(image) 
+
+        # preprocess
+        crop_image = preprocess(image_path, transform)
+        # predict
+        predict = test(model, crop_image)
+
+        st.write(idxtovowel[predict.item()])
