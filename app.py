24358dc064ef91ff1726f084eb2e8e2aebdf33d5
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-24 13:21:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import streamlit as st
 import numpy as np
+import cv2
+import math
 from PIL import Image
+import mediapipe as mp
+import subprocess
 import torch
 from torchvision import transforms
 
 import japanize_matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.cm
+
+
+# left eye contour
+landmark_left_eye_points = [133, 173, 157, 158, 159, 160, 161, 246, 33, 7, 163, 144, 145, 153, 154, 155]
+# right eye contour
+landmark_right_eye_points = [362, 398, 384, 385, 386, 387, 388, 466, 263, 249, 390, 373, 374, 380, 381, 382]
+
+size_LFROI = 200
 
 st.title("Streamlit App: Mouth shape recognition")
 st.write("Kyutech, Saitoh-lab")
@@ -16,7 +28,116 @@ target_person_id = st.selectbox("Target person", ("P001", "P002", "P003"))
 st.write("You selected:", target_person_id)
 st.markdown("---")
 
-def preprocess(image_path, transform):
+
+def pil2cv(image):
+    ''' PIL型 -> OpenCV型 '''
+    new_image = np.array(image, dtype=np.uint8)
+    if new_image.ndim == 2:
+        # モノクロ
+        pass
+    elif new_image.shape[2] == 3:
+        # カラー
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR)
+    elif new_image.shape[2] == 4:
+        # 透過
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGBA2BGRA)
+    
+    return new_image
+
+
+def func(value1, value2):
+    return int(value1 * value2)
+
+
+def LFROI_extraction(image, face_points0):
+    image_height, image_width, channels = image.shape[:3]
+
+    image_cx = image_width / 2
+    image_cy = image_height / 2
+
+    left_eye_x = face_points0[33][0]
+    left_eye_y = face_points0[33][1]
+    left_eye_point = (left_eye_x, left_eye_y)
+
+    right_eye_x = face_points0[263][0]
+    right_eye_y = face_points0[263][1]
+    right_eye_point = (right_eye_x, right_eye_y)
+
+    nose_x = face_points0[2][0]
+    nose_y = face_points0[2][1]
+    nose_point = (nose_x, nose_y)
+
+    eye_distance2 = (left_eye_x - right_eye_x) * (left_eye_x - right_eye_x) + (left_eye_y - right_eye_y) * (left_eye_y - right_eye_y)
+    eye_distance = math.sqrt(eye_distance2)
+
+    eye_angle = math.atan((left_eye_y - right_eye_y) / (left_eye_x - right_eye_x))
+
+    scale = 200.0 / eye_distance
+    cx = nose_x
+    cy = nose_y
+
+    mat_rot = cv2.getRotationMatrix2D((cx, cy), eye_angle, scale)
+    tx = image_cx - cx
+    ty = image_cy - cy
+    mat_tra = np.float32([[1, 0, tx], [0, 1, ty]])
+
+    image_width_ = int(image_width)
+    image_height_ = int(image_height)
+
+    normalized_image1 = cv2.warpAffine(image, mat_rot, (image_width_, image_height_))
+    normalized_image2 = cv2.warpAffine(normalized_image1, mat_tra, (image_width_, image_height_))
+
+    face_points1 = []
+    for p0 in face_points0:
+        x0 = p0[0]
+        y0 = p0[1]
+        x1 = mat_rot[0][0] * x0 + mat_rot[1][0] * y0 + mat_rot[0][2]
+        y1 = mat_rot[0][1] * x0 + mat_rot[1][1] * y0 + mat_rot[1][2]
+        x2 = x1 + mat_tra[0][2]
+        y2 = y1 + mat_tra[1][2]
+
+        face_points1.append((x2, y2))
+
+    cx = face_points1[2][0]
+    left = int(cx - size_LFROI / 2)
+    top = int(face_points1[2][1] - size_LFROI / 4)
+    right = left + size_LFROI
+    bottom = top + size_LFROI
+
+    return (left, top, right, bottom), normalized_image2, face_points1
+
+def preprocess(image):
+    black_image = np.zeros((size_LFROI, size_LFROI, 3), np.uint8)
+    white_image = black_image + 200
+
+    with mp.solutions.face_mesh.FaceMesh(
+        static_image_mode=True,
+        max_num_faces=1,
+        refine_landmarks=True,
+        min_detection_confidence=0.5
+    ) as face_mesh:
+
+        results = face_mesh.process(image)
+        (image_height, image_width) = image.shape[:2]
+      
+        if results.multi_face_landmarks:
+            for face in results.multi_face_landmarks:
+                points = []
+                for landmark in face.landmark:
+                    x = func(landmark.x, image_width)
+                    y = func(landmark.y, image_height)
+                    cv2.circle(out_image, center=(x, y), radius=2, color=(0, 255, 0), thickness=-1)
+                    cv2.circle(out_image, center=(x, y), radius=1, color=(255, 255, 255), thickness=-1)
+                    points.append((x, y))
+
+                rect_LFROI, normalized_image_LFROI, new_points_LFROI = faceROI_extraction(image, points)
+                LFROI = normalized_image_LFROI[rect_LFROI[1]: rect_LFROI[3], rect_LFROI[0]: rect_LFROI[2]]
+
+            return LFROI
+
+    return white_image
+    
+def preprocess_(image_path, transform):
     #########################################
     # Plese write extracting lipROI process #
     #########################################
@@ -30,10 +151,10 @@ def preprocess(image_path, transform):
 
 def make_graph(values):
     Y = np.arange(6)
-    #Y = ['N', 'a', 'i', 'u', 'e', 'o']
     X = values
     sm = matplotlib.cm.ScalarMappable(cmap='jet')
     fig, ax = plt.subplots()
+
     # 横棒グラフ
     ax.barh(Y, X, color=sm.to_rgba(X))
     ax.set_xlim(0, 1)
@@ -91,10 +212,11 @@ def main():
         image = Image.open(image_data)
         img_array = np.array(image)
         st.image(img_array, caption = 'uploaded image', use_column_width = None)
-        #input_image = pil2cv(image) 
+        input_image = pil2cv(image) 
 
         # preprocess
-        crop_image = preprocess(image_data, transform)
+        LFROI = preprocess(input_image)
+        crop_image = preprocess_(LFROI, transform)
         # predict
         predict = test(model, crop_image)
 
