3a2db1d0cfd5e80692b944422cbd113e4636abee
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-24 13:00:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -36,6 +36,7 @@ def make_graph(values):
     fig, ax = plt.subplots()
     # 横棒グラフ
     ax.barh(Y, X, color=sm.to_rgba(X))
+    ax.set_yticks([0, 1, 2, 3, 4, 5])
     ax.set_yticklabels(['閉', 'あ', 'い', 'う', 'え', 'お'] , size=14)
 
     st.pyplot(fig)
