9427069aa5edf644a6ccd72af2e5221b2cd1a42e
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-25 10:28:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,12 +21,8 @@ size_LFROI = 200
 size_graph_width = 200
 size_graph_height = 140
 
-st.title("Streamlit App: Mouth shape recognition")
+st.title("Six mouth shape recognition")
 st.write("Kyutech, Saitoh-lab")
-st.markdown("---")
-
-target_person_id = st.selectbox("Target person", ("P001", "P002", "P003"))
-st.write("You selected:", target_person_id)
 st.markdown("---")
 
 def pil2cv(image):
