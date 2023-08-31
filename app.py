7ae5de60c9aa8f4920661ea61e7bcd512e7067a5
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-31 12:54:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -303,11 +303,11 @@ class VideoProcessor:
         # 五つのmissing ScriptRunContext
         image_cv, LFROI_cv, is_detected_face = LFROI_extraction(image_cv)
 
-        #if self.is_mirroring == True:
-            #out_image_cv = cv2.flip(image_cv, 1)
-        #else:
-            #out_image_cv = image_cv.copy()
-        out_image_cv = image_cv.copy()
+        if self.is_mirroring == True:
+            out_image_cv = cv2.flip(image_cv, 1)
+        else:
+            out_image_cv = image_cv.copy()
+        #out_image_cv = image_cv.copy()
     
         #if is_detected_face == True:
             #out_image_cv[magrin:size_LFROI+magrin, magrin:size_LFROI+magrin] = LFROI_cv
