e8dea8670ce60ff4a648fe3ea90ebee2bffc54eb
Takeshi Saitoh <saitoh@ai.kyutech.ac.jp>
2023-08-31 23:17:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ size_graph_width = 200 # [pixel]
 size_graph_height = 140 # [pixel]
 previous_time = time.perf_counter() # [sec]
 
-st.title("LipWatch: Image-based real-time lip shape recognition")
+st.title("KuchiYomi: lip shape recognition")
 st.write("Kyutech, [Saitoh-lab](https://www.saitoh-lab.com/)")
 st.markdown("---")
 
